ffc5104bdf5e5cd105d3c4589c3993e2eee46e14
amiramohiey <ameramohiey92@gmail.com>
2017-04-25 17:04:15 +0200

    category favourite

--- bookshare/urls.py
+++ bookshare/urls.py
@@ -7,13 +7,14 @@ from library import views as library_views
 
 
 urlpatterns = [
-    url(r'^$', library_views.Header.as_view()),
+
     url(r'^admin/', admin.site.urls),
     url(r'^register/',views.UserRegisterationView.as_view(),name='register'),
     url(r'^login/',views.UserLoginView.as_view(),name='login'),
     url(r'^logout/', views.UserLogout.as_view(), name='logout'),
-    url(r'^home/', library_views.Home.as_view(), name='home'),
+    url(r'^$', library_views.Home.as_view(), name='home'),
     url(r'^categorydetails/(?P<pk>[0-9]+)/', library_views.CategoryDetails.as_view(), name='categorydetails'),
     url(r'^categorylist/', library_views.CategoryList.as_view(), name='categorylist')
+
 
 ]
--- library/views.py
+++ library/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic import View, DetailView
 from .models import Category
+from django.contrib.auth.models import User
 from django.http import JsonResponse,HttpResponse,Http404
-import json
+
 from django.core import serializers
 class Home(View):
     template_name = "home.html"
 
     def get(self, request):
         if request.user.is_authenticated():
-            return render(request, 'home.html')
+            category = Category.objects.filter(users=request.user)
+            return render(request, 'home.html',context={"category":category})
         else:
             return redirect('login')
 
@@ -20,12 +22,18 @@ class CategoryDetails(generic.DetailView):
     def get(self, request, pk):
 
         # books = Book.objects.filter(category_id=cat_id)
+
         try:
             category= Category.objects.get(pk=pk)
+
+            users=User.objects.all().filter(category=category)
+            print(category)
+            print(users)
+
         except Category.DoesNotExist:
             raise Http404("Category does not exist")
 
-        return render(request, 'categorydetails.html', context={"category": category, "books": "books"})
+        return render(request, 'categorydetails.html', context={"category": category, "users": users})
 
 
 class CategoryList(generic.ListView):
@@ -39,7 +47,6 @@ class CategoryList(generic.ListView):
         return HttpResponse(json_data)
 
 
-class Header(View):
-    def get(self, request):
-        category = Category.objects.all()
-        return render(request, 'header.html', context={"categories": category})
+
+
+        # return render(request, 'categorydetails.html', context={"category": category})
