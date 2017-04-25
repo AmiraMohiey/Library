305794fb591a333d60a4fc1bf3a27fcc8f75d87c
mhesham90 <mohammedhesham.1990@gmail.com>
2017-04-25 22:04:33 +0200

    modified book & author models + added their generic class views detail/list + modified their templates

--- bookshare/urls.py
+++ bookshare/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from users import views
 from library import views as library_views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -14,6 +15,9 @@ urlpatterns = [
     url(r'^logout/', views.UserLogout.as_view(), name='logout'),
     url(r'^home/', library_views.Home.as_view(), name='home'),
     url(r'^categorydetails/(?P<pk>[0-9]+)/', library_views.CategoryDetails.as_view(), name='categorydetails'),
-    url(r'^categorylist/', library_views.CategoryList.as_view(), name='categorylist')
-
-]
+    url(r'^categorylist/', library_views.CategoryList.as_view(), name='categorylist'),
+    url(r'^authors/', library_views.AuthorsList.as_view(), name='authors_list'),
+    url(r'^author/(?P<pk>[0-9]+)/', library_views.AuthorDetails.as_view(), name='author'),
+    url(r'^books/', library_views.BooksList.as_view(), name='books_list'),
+    url(r'^book/(?P<pk>[0-9]+)/', library_views.BookDetails.as_view(), name='book'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- library/views.py
+++ library/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from django.views.generic import View, DetailView
-from .models import Category
+from .models import Category, Author, Book
 from django.http import JsonResponse,HttpResponse,Http404
 import json
 from django.core import serializers
-class Home(View):
+
+
+class Home(generic.View):
     template_name = "home.html"
 
     def get(self, request):
@@ -39,7 +40,29 @@ class CategoryList(generic.ListView):
         return HttpResponse(json_data)
 
 
-class Header(View):
+class Header(generic.View):
     def get(self, request):
         category = Category.objects.all()
         return render(request, 'header.html', context={"categories": category})
+
+class AuthorsList(generic.ListView):
+    template_name = 'authors.html'
+    context_object_name = 'authors'
+
+    def get_queryset(self):
+        return Author.objects.all()
+
+class AuthorDetails(generic.DetailView):
+    model = Author
+    template_name = 'authordetails.html'
+
+class BooksList(generic.ListView):
+    template_name = 'books.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+class BookDetails(generic.DetailView):
+    model = Book
+    template_name = 'bookdetails.html'
