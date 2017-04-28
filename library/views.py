fc87f29bdf4a060722e32a6fd7ba8c2afe1d105d
amiramohiey <ameramohiey92@gmail.com>
2017-04-28 10:50:04 +0200

    home and category modifications

--- library/views.py
+++ library/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-
-from django.views.generic import View, DetailView
-from .models import Category,Book,Author
-from django.contrib.auth.models import User
-from django.http import JsonResponse, HttpResponse, Http404
-from django.db.models import Q
-
-
-from .models import Category, Author, Book
 from django.views.generic import View
 from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponse, Http404
-
+from django.views.generic import View, DetailView
+from django.contrib.auth.models import User
+from django.db.models import Q
+from .models import Category, Book, Author
 from django.core import serializers
+from django import template
+
 
 
 class Home(View):
@@ -21,8 +17,9 @@ class Home(View):
 
     def get(self, request):
         if request.user.is_authenticated():
+            last_five_books = Book.objects.all().order_by('-id')[:5]
             category = Category.objects.filter(users=request.user)
-            return render(request, 'home.html', context={"category": category})
+            return render(request, 'home.html', context={"category": category, "latestbooks": last_five_books})
         else:
             return redirect('login')
 
@@ -32,16 +29,15 @@ class CategoryDetails(generic.DetailView):
 
     def get(self, request, pk):
 
-        # books = Book.objects.filter(category_id=cat_id)
+        books = Book.objects.filter(category_id=pk)
 
         try:
             category = Category.objects.get(pk=pk)
-
             users = User.objects.all().filter(category=category)
 
         except Category.DoesNotExist:
             raise Http404("Category does not exist")
-        return render(request, 'categorydetails.html', context={"category": category, "users": users})
+        return render(request, 'categorydetails.html', context={"category": category, "users": users, "books": books})
 
     def post(self, request, pk):
         print("post", pk)
@@ -52,7 +48,6 @@ class CategoryDetails(generic.DetailView):
         return render(request, 'categorydetails.html', context={"category": category, "users": users})
 
 
-
 class RemoveFromFavourites(View):
     def post(self, request):
         pk = request.POST.get('pk')
@@ -61,7 +56,7 @@ class RemoveFromFavourites(View):
 
         print(pk)
         users = User.objects.all().filter(category=category)
-        return redirect('/categorydetails/'+pk, context={"category": category, "users": users})
+        return redirect('/categorydetails/' + pk, context={"category": category, "users": users})
 
 
 class CategoryList(generic.ListView):
@@ -80,6 +75,7 @@ class Header(generic.View):
         category = Category.objects.all()
         return render(request, 'header.html', context={"categories": category})
 
+
 class AuthorsList(generic.ListView):
     template_name = 'authors.html'
     context_object_name = 'authors'
@@ -87,32 +83,35 @@ class AuthorsList(generic.ListView):
     def get_queryset(self):
         return Author.objects.all()
 
+
 class AuthorDetails(generic.DetailView):
     model = Author
     template_name = 'authordetails.html'
 
+
 class BooksList(generic.ListView):
-    model=Book
     template_name = 'books.html'
     context_object_name = 'books'
 
+    def get_queryset(self):
+        return Book.objects.all()
+
 
 class Search(View):
-    def post(self,request):
-        search_query=request.POST.get('search_query')
+    def post(self, request):
+        search_query = request.POST.get('search_query')
         qset_book = Q()
-        qset_author= Q()
+        qset_author = Q()
         for term in search_query.split():
             qset_book |= Q(title__icontains=term)
-
 
         books = Book.objects.filter(qset_book)
         for term in search_query.split():
             qset_author |= Q(name__icontains=term)
-        authors= Author.objects.filter(qset_author)
+        authors = Author.objects.filter(qset_author)
         return render(request, 'searchresults.html', context={"books": books, "authors": authors})
 
-    def get(self, request,search_query):
+    def get(self, request, search_query):
         qset_book = Q()
         qset_author = Q()
         for term in search_query.split():
@@ -123,16 +122,11 @@ class Search(View):
             qset_author |= Q(name__icontains=term)
         authors = Author.objects.filter(qset_author)
 
-
         print(search_query)
 
         json_books = serializers.serialize('json', books)
-        json_authors=serializers.serialize('json', authors)
-        return JsonResponse(json_books,safe=False)
-
-
-    def get_queryset(self):
-        return Book.objects.all()
+        json_authors = serializers.serialize('json', authors)
+        return JsonResponse(json_books, safe=False)
 
 
 class BookDetails(generic.DetailView):
